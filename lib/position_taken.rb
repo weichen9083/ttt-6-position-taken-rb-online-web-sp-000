c6c05c75c6a3ea893b9ccc9d0e58133c4f3d794b
wei chen <weichen9083@gmail.com>
2019-08-21 02:09:21 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  a = index
-  if board[a] == " " || board[a] =="" || board[a] == nil 
+ index
+  if board[index] == " " || board[index] =="" || board[index] == nil 
     return false
 
   else 

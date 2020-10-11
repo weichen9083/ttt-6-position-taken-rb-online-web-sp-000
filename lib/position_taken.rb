e1cc8149a5aaac4e6e6f2e0aee34e7b3d9d10d1d
wei chen <weichen9083@gmail.com>
2020-10-11 02:05:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,16 +1,11 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
- 
-  if board[index] == " " || board[index] =="" || board[index] == nil 
   
-  false
-
-  else 
-  true
+  if board[index] == "X" || board[index] == "O"
+    true 
+  else
+    false 
   end 
-  
-  
-  
+ 
 end 
-

daf012a5775dc66be9e052ee9bc4c5ae0d83ab61
wei chen <weichen9083@gmail.com>
2020-10-11 01:59:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,13 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
- index
+ 
   if board[index] == " " || board[index] =="" || board[index] == nil 
-    return false
+  
+  false
 
   else 
-    return true
+  true
   end 
   
   
